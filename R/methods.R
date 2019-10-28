e9ef7e4ca6eb3dd081ec1be6f9cbf12cde6dd556
elad <eladg9@gmail.com>
2019-10-28 19:42:38 +0200

    add nn function

--- R/methods.R
+++ R/methods.R
@@ -5,30 +5,34 @@
 #'
 #' Calculates weights under I = 1, using the non-parametric method
 #'
-#' @param Y outcome
+#' @inheritParams optint
 #'
 #' @return vector of weights under I = 1
 
-non_parm <- function(Y, X, controls, wgt = NULL, lambda = 100){
+non_parm <- function(Y, X, controls = NULL, wgt = NULL, lambda = 100, ...){
   if (is.null(wgt)){
     wgt <- rep(1,length(Y))
   }
-  #basline weight (without controls)
+  #basline weights (without controls)
   base_wgt1 <- wgt * Y^(1/lambda)
-  #add a constant vector to controls
-  controls <- as.matrix(cbind(rep(1,nrow(controls)), controls))
-  #define function to solve, plug in variables
-  f <- function(b) {dev_moments(b, base_wgt1, controls, wgt)}
-  #get solution
-  b0 <- rootSolve::multiroot(f, rep(0,ncol(controls)))$root
-  #calculate probs with solution
-  wgt1 <- base_wgt1 * exp(controls %*% as.matrix(b0))
-  return(wgt1)
+  if (is.null(controls)){
+    return(base_wgt1)
+  } else {
+    #add a constant vector to controls
+    controls <- as.matrix(cbind(rep(1,nrow(controls)), controls))
+    #define function to solve, plug in variables
+    f <- function(b) {dev_moments(b, base_wgt1, controls, wgt)}
+    #get solution
+    b0 <- rootSolve::multiroot(f, rep(0,ncol(controls)))$root
+    #calculate probs with solution
+    wgt1 <- base_wgt1 * exp(controls %*% as.matrix(b0))
+    return(wgt1)
+  }
 }
 
 #' Moment deviation
 #'
-#' Find the moment deviation for a given lagrange multiplier
+#' Finds the moment deviation for a given lagrange multiplier
 #'
 #' @param beta a lagrange multiplier
 #' @param base basline weights
@@ -37,10 +41,42 @@ non_parm <- function(Y, X, controls, wgt = NULL, lambda = 100){
 #'
 #' @return vector of moment deviations
 
-dev_moments <- function(beta, base, controls, wgt){
+dev_moments <- function(beta, base, controls, wgt, ...){
   #calculate probs for a given beta
   p <- base * exp(controls %*% as.matrix(beta))
   #calculate the difference in means between the two distributions
   dev <- apply(as.vector(p - wgt) * controls, 2, sum)
   return(dev)
+}
+
+#' Nearest-neighbors method
+#'
+#' Calculates weights under I = 1, using the nearest-neighbors method
+#'
+#' @inheritParams optint
+#'
+#' @return vector of weights under I = 1
+
+nn <- function(Y, X, controls = NULL, wgt = NULL, lambda = 100, sigma = 1, ...){
+  if (is.null(wgt)){
+    wgt <- rep(1,length(Y))
+  }
+  char_matrix <- cbind(X, controls)
+  vcov <- cov.wt(char_matrix, wgt)$cov
+  #find mehalanobis distance for all pairs
+  dist <- distances::distances(char_matrix, normalize = vcov)
+  #log weights matrix (log is for precision)
+  lwgt_mat <- -.5 * (dist^2) * (sigma^-2) + (1/lambda) * log(Y) + log(wgt)
+  #make max 100 for precision
+  lwgt_mat <- lwgt_mat - max(lwgt_mat) + 100
+  wgt_mat <- exp(lwgt_mat)
+  #now make wgt_mat_ij sum to wgt for every column (i) (j is the raw index)
+  #first, make each column (i) sum to 1
+  si <- apply(wgt_mat, 2, sum)
+  wgt_mat <-  t(t(wgt_mat) / si)
+  #second, make each column sum to the sampling weight of that column (i)
+  wgt_mat <- t(t(wgt_mat) * wgt)
+  #sum rows (over i) and that's the overall weight on each observation
+  wgt1 = apply(wgt_mat, 1, sum)
+  return(wgt1)
 }
--- tests/testthat/test_methods_functions.R
+++ tests/testthat/test_methods_functions.R
@@ -2,18 +2,30 @@ library(optinterv)
 
 set.seed(123)
 
-test_that("check if controls were changed", {
-  #create data
-  p        <- 10
-  j        <- 3
-  n        <- 500
-  vars     <- matrix(2 * runif(n * p), n, p)
-  controls <- 3 * matrix(runif(n * j), n, j)
-  outcome  <- rnorm(n) + apply(X, 1, sum) + X[,1]
+#create data
+p        <- 10
+j        <- 3
+n        <- 500
+vars     <- matrix(2 * runif(n * p), n, p)
+controls <- 3 * matrix(runif(n * j), n, j)
+outcome  <- rnorm(n) + apply(controls, 1, sum) + vars[,1]
+
+test_that("controls were changed using the non-parametric method", {
   #calculate means
   wgt1 <- non_parm(outcome, vars, controls)
-  c1   <- apply(controls ,2 ,function(v) weighted.mean(v,wgt1,na.rm = T))
-  c0   <- apply(controls, 2, function(v) mean(v,na.rm = T))
+  c1   <- apply(controls ,2 ,function(v) weighted.mean(v, wgt1))
+  c0   <- apply(controls, 2, mean)
   expect_true(sum(abs(c1 - c0)) < .000001)
 })
+
+
+test_that("controls were changed using the nearest-neighbors method", {
+  #calculate means
+  wgt1 <- nn(outcome, vars, controls)
+  c1   <- apply(controls ,2 ,function(v) weighted.mean(v, wgt1))
+  c0   <- apply(controls, 2, mean)
+  expect_true(sum(abs(c1 - c0)) < .000001)
+})
+
+#write test for "reasonable" results
 
